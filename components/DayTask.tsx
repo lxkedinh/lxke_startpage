4dc21b58b172e0806801f5efb5d2481bae07e2ed
Luke Dinh <lukexdinh@gmail.com>
2023-06-19 17:28:25 -0700

    fix: tailwind dynamic classes now working - different types of todo list tasks now have different color labels

--- components/DayTask.tsx
+++ components/DayTask.tsx
@@ -8,6 +8,20 @@ interface Props {
 }
 
 const DayTask: FunctionComponent<Props> = ({ time, title, label, url }) => {
+  // this must be defined inside component because otherwise tailwind won't
+  // detect and include these class names in output css bundle
+  const colors: Record<string, string> = {
+    "Object Oriented Programming": "text-ctp-flamingo",
+    "Computers and Society": "text-ctp-pink",
+    "Computer Architecture": "text-ctp-lavender",
+    "Numerical Methods": "text-ctp-green",
+    Ethics: "text-ctp-yellow",
+    work: "text-ctp-red",
+    icebreak: "text-ctp-peach",
+    personal: "text-ctp-sapphire",
+    school: "text-ctp-teal",
+  };
+
   return (
     <li className="font-[Kubasta] text-lg 2xl:text-xl/5 px-1 pb-1 text-ctp-text hover:bg-ctp-mantle [line-height:1rem] mb-2">
       <a href={url}>
@@ -22,15 +36,3 @@ const DayTask: FunctionComponent<Props> = ({ time, title, label, url }) => {
 };
 
 export default DayTask;
-
-const colors: Record<string, string> = {
-  "Object Oriented Programming": "text-ctp-flamingo",
-  "Computers and Society": "text-ctp-pink",
-  "Computer Architecture": "text-ctp-lavender",
-  "Numerical Methods": "text-ctp-green",
-  Ethics: "text-ctp-yellow",
-  work: "text-ctp-red",
-  icebreak: "text-ctp-peach",
-  personal: "text-ctp-sapphire",
-  school: "text-ctp-teal",
-};
