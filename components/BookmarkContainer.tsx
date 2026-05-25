ae1eac441aaf5610bc52b20ea87ff01bfb78cbd4
Luke Dinh <lukexdinh@gmail.com>
2026-05-25 10:14:17 -0700

    feat: changed bookmark container title

--- components/BookmarkContainer.tsx
+++ components/BookmarkContainer.tsx
@@ -8,7 +8,7 @@ const BookmarkContainer = () => {
     <div className="flex flex-row w-full items-center h-[250px] 2xl:h-[350px]">
       <div className="flex flex-col justify-center text-center flex-grow mr-1 items-center bg-ctp-base h-full">
         <h1 className="font-[Kubasta] text-ctp-text text-2xl mb-5">
-          ジャスミン
+          よく生きる
         </h1>
         <div className="flex flex-row items-center justify-center">
           {allBookmarks.map((bookmarkSection, index) => (
